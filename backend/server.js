7a40078e7f59abf309abfd7b25bf968e5e28b204
Victor-Leung-CA <victor.leung@alumni.ubc.ca>
2021-02-22 22:42:57 -0800

    minor updates

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-//Initialise .env file
-require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 //Initialise express app, use cors and express module
@@ -11,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Initialise .env file
+require('dotenv').config();
 //Set up default mongoose connection
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
