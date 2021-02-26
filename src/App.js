44c1cedfd17cf0e0c21d5d1ba73d1196de68ea93
Victor-Leung-CA <victor.leung@alumni.ubc.ca>
2021-02-25 16:35:50 -0800

    IFR Report updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
-
 import React, {Component} from "react";
-import{
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import{ Route, NavLink, BrowserRouter, Switch } from "react-router-dom";
+
+// Page imports
 import Home from "./Home";
 import AlertHistory from "./AlertHistory";
 import Gallery from "./Gallery";
 import Map from "./Map";
 import NewIFR from "./NewIFR";
+import IFRForm from "./components/IFR-form";
 import SSETest from "./components/sse-test"
 
 class App extends Component {
   render() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="App">
     
         <h1> FireDrone </h1>
@@ -26,21 +24,25 @@ class App extends Component {
           <li><NavLink to="/alert-history">Alert History</NavLink></li>
           <li><NavLink to="/new-ifr">Create New IFR</NavLink></li>
           <li><NavLink to="/gallery">Gallery</NavLink></li>
+          <li><NavLink to="/sse-test">Real Time Data</NavLink></li>
         </ul>
         
-        <div className="content">
-          <Route exact path="/" component={Home}/>
-          <Route path="/map" component={Map}/>
-          <Route path="/alert-history" component={AlertHistory}/>
-          <Route path="/new-ifr" component={NewIFR}/>
-          <Route path="/gallery" component={Gallery}/>
 
-          {/* For testing real time updates */}
-          <Route path="/sse-test" component = {SSETest}/>
+        {/* Routes */}
+        <Route exact path="/" component={Home}/>
+        <Route path="/map" component={Map}/>
+        <Route path="/alert-history" component={AlertHistory}/>
+        <Route path="/new-ifr" component={NewIFR}/>
+        <Route path="/gallery" component={Gallery}/>
+
+        {/* IFR paths for different incident nums */}
+        <Route path="/edit-ifr/:incidentNum" component = {IFRForm}/>
+
+        {/* For testing real time updates */}
+        <Route path="/sse-test" component = {SSETest}/>
           
-        </div>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   ); }
 }
 
