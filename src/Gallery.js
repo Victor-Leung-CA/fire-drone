89292104ad02eecd7048224d9ee8ec38d349e421
Victor-Leung-CA <victor.leung@alumni.ubc.ca>
2021-03-14 17:17:56 -0700

    Gallery updates

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,6 @@
-npm install react-image-gallery
-@import "react-image-gallery/styles/css/image-gallery.css";
-
+import React, {Component} from "react";
 import ImageGallery from 'react-image-gallery';
+import "./css/Gallery.css"
 
 const images = [
  {
