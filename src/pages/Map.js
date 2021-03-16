e8ae4a2e3ef51a9146268154338ad28cf307f2d3
Toma La Cara <72001918+tlacara@users.noreply.github.com>
2021-03-16 13:20:33 -0700

    Update Map.js

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -11,4 +11,33 @@ const Map = () => {
   )
 }
 
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAChc-w8pscsRgaW5t1S2jyHLNzRVpOO9c' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
 export default Map;
