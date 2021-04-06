22c09cb247e40aa0714753e163397b72b92e65fa
Toma La Cara <72001918+tlacara@users.noreply.github.com>
2021-04-06 13:26:54 -0700

    included gps coordinates from sensordata

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -3,7 +3,8 @@ import '../css/Map.css';
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const getSensorData = require("../API/sensorData").getSensorData;
+
 
 class Map extends Component {
   static defaultProps = {
@@ -14,20 +15,44 @@ class Map extends Component {
     zoom: 11
   };
 
+
+  constructor(props){
+    super(props);
+    this.state = {data: [[{longitude: -123.1207,latitude: 49.2827}]]}
+    var coordinates = []
+
+    getSensorData().then(item => {
+      item.data.map(alert => {
+        coordinates.push(alert.coordinates)
+      })
+      this.state.data=coordinates
+      console.log(this.state.data)
+      this.forceUpdate();
+      console.log("updated")
+      console.log(this.state.data[0][0].longitude)
+    })
+
+  }
+
+
+
   render() {
     return (
       <div className="mapContainer">
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAChc-w8pscsRgaW5t1S2jyHLNzRVpOO9c' }}
-          defaultCenter={this.props.center}
+          defaultCenter={{lat: this.state.data[0][0].latitude, lng: this.state.data[0][0].longitude} }
           defaultZoom={this.props.zoom}
         >
-          <LocationMarker
-            lat={49.2827}
-            lng={-123.1207}
-            
-          />
-          
+                    
+          { 
+            this.state.data.map(item =>{
+              console.log(item)
+              return( <LocationMarker lat={item[0].latitude} lng={item[0].longitude} />)
+            }) 
+          }
+                                               
+                       
         </GoogleMapReact>
       </div>
     );
